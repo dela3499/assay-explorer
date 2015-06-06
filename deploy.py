7e0ef4d38ca9117aa3d4fa3767724ff88be49c94
cdelaguardia <dela3499@gmail.com>
2015-06-06 16:01:51 -0700

    Added deploy scripts.

---
+++ deploy.py
@@ -0,0 +1,6 @@
+import sh
+from toolz import thread_first, compose
+
+update_path = "/home/ubuntu/prod/assay-explorer/update_server.py"
+server = sh.ssh.bake('-i','/my-pair.pem','ubuntu@52.8.164.123')
+server.python(update_path)
