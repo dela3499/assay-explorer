24f54e3c070adc5419dc12c727e9a782b319c630
Daniel Wuttke <hevok@denigma.de>
2016-05-09 13:58:41 -0700

    Auto confirm install.

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
-sudo apt-get install build-essential
-sudo apt-get install python-dev
-sudo apt-get install gcc
-sudo apt-get install g++
-sudo apt-get install libzmq3 libzmq3-dev
+sudo apt-get install -y build-essential
+sudo apt-get install -y python-dev
+sudo apt-get install -y gcc
+sudo apt-get install -y g++
+sudo apt-get install -y libzmq3 libzmq3-dev
 
-sudo apt-get install libfreetype6-dev libxft-dev # for matplotlib
-sudo apt-get install gfortran libopenblas-dev liblapack-dev # numpy
-
-
-sudo apt-get install libfreetype6-dev libfreetype6
+sudo apt-get install -y libfreetype6-dev libxft-dev # for matplotlib
+sudo apt-get install -y gfortran libopenblas-dev liblapack-dev # numpy
