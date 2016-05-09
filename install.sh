b895f71869e9f430e50a166ed2a7ee3f41fee11d
Daniel Wuttke <hevok@denigma.de>
2016-05-09 13:55:23 -0700

    Added install bash script.

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt-get install build-essential
+sudo apt-get install python-dev
+sudo apt-get install gcc
+sudo apt-get install g++
+sudo apt-get install libzmq3 libzmq3-dev
+
+sudo apt-get install libfreetype6-dev libxft-dev # for matplotlib
+sudo apt-get install gfortran libopenblas-dev liblapack-dev # numpy
+
+
+sudo apt-get install libfreetype6-dev libfreetype6
